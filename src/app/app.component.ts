0a73be4beed4c3bce0b2781756c9a7daa6130c90
Piotr Dylla <piotrdylla@gmail.com>
2022-02-27 19:51:11 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'piotrdylla';
+export class AppComponent implements OnInit {
+
+  images = ['computer.jpg', 'computer1.jpg'];
+
+  ngOnInit() {
+
+  }
+
+  updateImage() {
+
+
+  }
+  
+
+
 }
